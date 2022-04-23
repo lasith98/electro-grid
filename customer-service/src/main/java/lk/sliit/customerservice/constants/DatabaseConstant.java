6f32b61131c06a40028ad1bf0ac7e1757e1f74f8
Lasith Hansana <lasithhansana9@gmail.com>
2022-04-23 21:55:07 +0530

    fix sonar lit issue

--- customer-service/src/main/java/lk/sliit/customerservice/constants/DatabaseConstant.java
+++ customer-service/src/main/java/lk/sliit/customerservice/constants/DatabaseConstant.java
@@ -2,8 +2,12 @@ package lk.sliit.customerservice.constants;
 
 public class DatabaseConstant {
 
-    public static String DATABASE_URL = "database.url";
-    public static String DATABASE_DRIVER_NAME = "database.driver.name";
-    public static String DATABASE_USERNAME = "database.username";
-    public static String DATABASE_PASSWORD = "database.password";
+    private DatabaseConstant() {
+
+    }
+
+    public static final String DATABASE_URL = "database.url";
+    public static final String DATABASE_DRIVER_NAME = "database.driver.name";
+    public static final String DATABASE_USERNAME = "database.username";
+    public static final String DATABASE_PASSWORD = "database.password";
 }
