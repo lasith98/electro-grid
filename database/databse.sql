b1549a758b5212e57136cc2c9cc53030fd462314
Lasith Hansana <lasithhansana9@gmail.com>
2022-04-26 21:18:31 +0530

    add database file

---
+++ database/databse.sql
@@ -0,0 +1,159 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for macos11 (x86_64)
+--
+-- Host: localhost    Database: electro_grid
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `billing`
+--
+
+DROP TABLE IF EXISTS `billing`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `billing` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `customer_id` int DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `unit` float DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `billing`
+--
+
+LOCK TABLES `billing` WRITE;
+/*!40000 ALTER TABLE `billing` DISABLE KEYS */;
+INSERT INTO `billing` VALUES (2,2,'2022-07-01',49,595),(3,2,'2022-07-01',49,595);
+/*!40000 ALTER TABLE `billing` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer_care`
+--
+
+DROP TABLE IF EXISTS `customer_care`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer_care` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `customerId` int DEFAULT NULL,
+  `bilAccount` int DEFAULT NULL,
+  `complainType` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer_care`
+--
+
+LOCK TABLES `customer_care` WRITE;
+/*!40000 ALTER TABLE `customer_care` DISABLE KEYS */;
+INSERT INTO `customer_care` VALUES (2,2,23,'Break down');
+/*!40000 ALTER TABLE `customer_care` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `maintain`
+--
+
+DROP TABLE IF EXISTS `maintain`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `maintain` (
+  `ID` int NOT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `affected_Area` varchar(45) DEFAULT NULL,
+  `affected_time` varchar(45) DEFAULT NULL,
+  `predicted_time` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `maintain`
+--
+
+LOCK TABLES `maintain` WRITE;
+/*!40000 ALTER TABLE `maintain` DISABLE KEYS */;
+INSERT INTO `maintain` VALUES (105,'Maintainance due low voltage','Jaffna','2022-04-29','1 hours starting from 07.00 P.M');
+/*!40000 ALTER TABLE `maintain` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `onlinepayment`
+--
+
+DROP TABLE IF EXISTS `onlinepayment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `onlinepayment` (
+  `name` varchar(20) DEFAULT NULL,
+  `id` int DEFAULT NULL,
+  `amount` int DEFAULT NULL,
+  `cardtype` varchar(20) DEFAULT NULL,
+  `bank` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `onlinepayment`
+--
+
+LOCK TABLES `onlinepayment` WRITE;
+/*!40000 ALTER TABLE `onlinepayment` DISABLE KEYS */;
+INSERT INTO `onlinepayment` VALUES ('Nimal',1,1000,'Visa','BOC'),('Kamal',2,2000,'Visa','HNB'),('Sunil',3,3000,'Master','Com');
+/*!40000 ALTER TABLE `onlinepayment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `power_usage`
+--
+
+DROP TABLE IF EXISTS `power_usage`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `power_usage` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `customer_id` int DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `unit` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `power_usage`
+--
+
+LOCK TABLES `power_usage` WRITE;
+/*!40000 ALTER TABLE `power_usage` DISABLE KEYS */;
+INSERT INTO `power_usage` VALUES (2,2,'2020-07-01',49),(3,2,'2022-07-01',49),(4,2,'2022-07-01',49);
+/*!40000 ALTER TABLE `power_usage` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-26 21:17:12
